d0b5562aa9a436895e556ba84ea413c8f81b15bb
PLLima <74931964+PLLima@users.noreply.github.com>
2023-11-16 09:28:56 -0300

    Renamed exercises

--- Natural_Deduction/List_2/Main.lean
+++ Natural_Deduction/List_2/Main.lean
@@ -1,14 +1,16 @@
 variable (p q r s : Prop)
 
-theorem ex1 : (p ∧ q) → (p ∨ q) := by
+theorem ex_a : (p ∧ q) → (p ∨ q) := by
   intro h1
   apply Or.inl
   apply And.left
   exact h1
 
-theorem ex2 (h1 : ¬(p ∨ q)) : (¬p ∨ ¬q) := by
+theorem ex_e (h1 : ¬(p ∨ q)) : (¬p ∨ ¬q) := by
   apply Or.inl
   intro h2
   have h3 : p ∨ q := Or.inl h2
   apply h1
   exact h3
+
+theorem ex_g (h1 : ¬(p ∧ q)) : (¬p ∨ ¬q) := sorry
